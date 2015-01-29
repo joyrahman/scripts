4afd28b79102f3cf6ad6db7df71b0a39be0577b1
joyrahman <joyrahman@gmail.com>
2015-01-29 15:56:08 -0600

    added

--- billing_report.py
+++ billing_report.py
@@ -7,7 +7,7 @@ def print_report(report):
 
 def write_to_csv(csv_data):
 
-    directory_name = "~/report"
+    directory_name = "report"
     time_format = '%Y%m%d_%H%M%S'
     current_time = time.strftime(time_format)
     file_extension = "csv"
