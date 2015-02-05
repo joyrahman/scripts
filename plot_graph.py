58a3c55cc2bc2fc06e7eac1dd75b04811144f6f9
joyrahman <joyrahman@gmail.com>
2015-02-05 11:18:15 -0600

    added

---
+++ plot_graph.py
@@ -0,0 +1,28 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+import sys
+
+
+
+def main(file_name):
+
+    with open(file_name, 'rb') as f:
+        print "opening {}".format(file_name)
+        reader = csv.reader(f)
+        data = list(reader)
+
+    for item in data:
+        start_time = item[0]
+        end_time   = item[1]
+        job_id     = item[2]
+        container_id = item[3]
+        execution_time = item[4]
+        plt.hlines(job_id,start_time, end_time, colors = 'red')
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    main(sys.argv[1])
+
