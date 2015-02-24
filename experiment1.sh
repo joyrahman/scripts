635e2a897b9190edd371aa50374cebbb1c28f892
joyrahman <joyrahman@gmail.com>
2015-02-23 19:56:32 -0600

    Create experiment1.sh

---
+++ experiment1.sh
@@ -0,0 +1,7 @@
+# varaible is interval 
+python experiment_exp1_loadlevel.py 1 500 -1 test1.csv
+python experiment_exp1_loadlevel.py 2 500 -1 test1.csv
+python experiment_exp1_loadlevel.py 3 500 -1 test1.csv
+python experiment_exp1_loadlevel.py 4 500 -1 test1.csv
+python experiment_exp1_loadlevel.py 5 500 -1 test1.csv
+
