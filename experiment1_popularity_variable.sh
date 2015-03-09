300514de176d3a6e490d9a11079dbf2906bdddf2
joyrahman <joyrahman@gmail.com>
2015-03-09 20:36:17 +0000

    last commit wq

--- experiment1_popularity_variable.sh
+++ experiment1_popularity_variable.sh
@@ -4,37 +4,38 @@ output_file_name="var_popularity"
 popularity=-1
 interval=1
 end_limit=5
-executable="experiment_exp1_loadlevel.py"
-no_of_sessions=5
+#executable="experiment_exp1_loadlevel.py"
+executable="experiment_exp1_popularity.py"
+no_of_sessions=200
 
 for i in 0 10 20 30 40 50 60 70 80 90 100
 do
    echo "running iteration:$i, $j"
-   python $executable 1 $no_of_sessions $i "$output_file_name_1_pop$i"
+   python $executable 1 $no_of_sessions $i "$output_file_name-1_pop$i"
 done
 
 for i in 0 10 20 30 40 50 60 70 80 90 100
 do      
    echo "running iteration:$i, $j"
-   python $executable 2 $no_of_sessions $i "$output_file_name_1_pop$i"
+   python $executable 2 $no_of_sessions $i "$output_file_name-2_pop$i"
 done
 
 for i in 0 10 20 30 40 50 60 70 80 90 100
 do      
    echo "running iteration:$i, $j"
-   python $executable 3 $no_of_sessions $i "$output_file_name_1_pop$i"
+   python $executable 3 $no_of_sessions $i "$output_file_name-3_pop$i"
 done
 
 for i in 0 10 20 30 40 50 60 70 80 90 100
 do      
    echo "running iteration:$i, $j"
-   python $executable 4 $no_of_sessions $i "$output_file_name_1_pop$i"
+   python $executable 4 $no_of_sessions $i "$output_file_namer-4_pop$i"
 done
 
 for i in 0 10 20 30 40 50 60 70 80 90 100
 do      
    echo "running iteration:$i, $j"
-   python $executable 5 $no_of_sessions $i "$output_file_name_1_pop$i"
+   python $executable 5 $no_of_sessions $i "$output_file_name-5_pop$i"
 done
 
 
