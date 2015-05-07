f63af1a80056b8c694075afb9a4f7dc856028b4a
joyrahman <joyrahman@gmail.com>
2015-05-07 14:18:06 -0500

    updaed automated_job2.py

--- experiment1_popularity_variable.sh
+++ experiment1_popularity_variable.sh
@@ -6,9 +6,9 @@ interval=10
 end_limit=5
 #executable="experiment_exp1_loadlevel.py"
 executable="experiment_exp1_popularity.py"
-no_of_sessions=5
+no_of_sessions=1
 
-for i in 0 10 20 30 40 50 60 70 80 90 100
+for i in 0 50 80
 do
    echo "running iteration:$i, $j"
    python $executable 1 $no_of_sessions $i "$output_file_name-1_pop$i"
